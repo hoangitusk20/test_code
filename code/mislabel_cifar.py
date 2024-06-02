6df782d94064c2a874415c71945a06348f77eeb7
hoangitusk20 <hoangitusk20@gmail.com>
2024-06-02 07:35:51 +0700

    Fix correct label

--- code/mislabel_cifar.py
+++ code/mislabel_cifar.py
@@ -107,6 +107,14 @@ class MISLABELCIFAR10(torchvision.datasets.CIFAR10):
         count_dict = dict(zip(unique_values, counts))
 
         print(count_dict)
+    def print_real_class_dis(self):
+        targets_np = np.array(self.real_targets)
+        unique_values, counts = np.unique(targets_np, return_counts=True)
+
+        # Combine the results into a dictionary
+        count_dict = dict(zip(unique_values, counts))
+        print(count_dict)
+
 
     def fetch(self, targets): # Chọn ra data từ target, sample đúng với target nhưng lấy ngẫu nhiên (Dòng 109)
         whole_targets_np = np.array(self.whole_targets)
