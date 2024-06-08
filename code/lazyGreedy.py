9d5c96ff09180dd9f3f10fbdc24c1ae67568b8c9
Huy đẹp trai <86175306+HuyHCMUS@users.noreply.github.com>
2024-06-08 21:12:46 +0700

    Update lazyGreedy.py

--- code/lazyGreedy.py
+++ code/lazyGreedy.py
@@ -28,7 +28,6 @@ def lazy_greedy_heap(F, V, B):
     heapq._heapify_max(order)
 
     # Khởi tạo heap là tổng khoảng cách từ 1 điểm đến tất cả các điểm còn lại
-    print("initializing heap")
     for index in V:
         _heappush_max(order, (F.inc(sset, index), index))
     print("finsh!")
