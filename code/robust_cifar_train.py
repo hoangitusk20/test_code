e65e268b3176f58abf31517c224624df2ced1575
Huy đẹp trai <86175306+HuyHCMUS@users.noreply.github.com>
2024-06-23 22:27:32 +0700

    Update robust_cifar_train.py

--- code/robust_cifar_train.py
+++ code/robust_cifar_train.py
@@ -20,7 +20,7 @@ import copy
 from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import grad
 from fl_cifar import FacilityLocationCIFAR
-from lazyGreedy import lazy_greedy_heap, algo1
+from lazyGreedy import lazy_greedy_heap, algo1, k_medoids
 from utils import *
 from mislabel_cifar import MISLABELCIFAR10, MISLABELCIFAR100
 
